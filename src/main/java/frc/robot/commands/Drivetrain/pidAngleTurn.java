e9cd2a7995db2932471b8fbde2b6cf4d58267d31
Ç*** F*** <61086421+MorphaxTheDeveloper@users.noreply.github.com>
2023-01-22 22:51:26 +0300

    pid angle turn update

--- src/main/java/frc/robot/commands/Drivetrain/pidAngleTurn.java
+++ src/main/java/frc/robot/commands/Drivetrain/pidAngleTurn.java
@@ -9,47 +9,31 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.robot.subsystems.DriveSubsystem;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class pidAngleTurn extends PIDCommand {
-  /** Creates a new pidAngleTurn. */
-  DriveSubsystem m_drive;
-  double angle;
 
   public pidAngleTurn(DriveSubsystem m_drive, double angle) {
     super(
-        // The controller that the command will use
-        new PIDController(0.012, 0.0005, 0.0040),
-        // This should return the measurement
+        new PIDController(1, 0, 0),
         () -> m_drive.GetHeading(),
-        // This should return the setpoint (can also be a constant)
         () -> angle,
-        // This uses the output
+
         output -> {
-          // Use the output here
           if (m_drive.GetHeading() > angle) {
-            m_drive.arcadeDrive(0, Math.max(-output, -0.30));
-          } else {
-            m_drive.arcadeDrive(0, Math.min(-output, 0.30));
+            m_drive.arcadeDrive(Math.max(-output, -0.3),0);
+
+            //outputtaki "-"leri gözden geçir!
+          } else if(angle > m_drive.GetHeading()) {
+            m_drive.arcadeDrive(Math.min(output, 0.3) ,0);
           }
 
           SmartDashboard.putNumber("output", -output);
           SmartDashboard.putNumber("angle", angle);
           SmartDashboard.putNumber("heading", m_drive.GetHeading());
 
-
-
         });
-    // Use addRequirements() here to declare subsystem dependencies.
-    // Configure additional PID options by calling `getController` here.
-    this.m_drive = m_drive;
-    this.angle = angle;
-    addRequirements(m_drive);
+        //getController().setTolerance(1.5,1.5);
   }
 
-
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return getController().atSetpoint();
