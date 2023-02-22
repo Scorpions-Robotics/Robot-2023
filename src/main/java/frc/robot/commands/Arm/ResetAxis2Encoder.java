0dc10456854ceefa6a5789bb0fa8b540f12c572d
Ç*** F*** <61086421+MorphaxTheDeveloper@users.noreply.github.com>
2023-02-23 00:47:06 +0300

    new reset commands

--- src/main/java/frc/robot/commands/Arm/ResetAxis2Encoder.java
+++ src/main/java/frc/robot/commands/Arm/ResetAxis2Encoder.java
@@ -7,27 +7,24 @@ package frc.robot.commands.Arm;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ArmSubsystem;
 
-public class ResetAxis1Encoder extends CommandBase {
-  /** Creates a new ResetAxis1Encoder. */
+public class ResetAxis2Encoder extends CommandBase {
+  /** Creates a new ResetAxis2Encoder. */
   ArmSubsystem m_arm;
-  public ResetAxis1Encoder(ArmSubsystem m_arm) {
-    this.m_arm = m_arm;
+  
+  public ResetAxis2Encoder(ArmSubsystem m_arm) {
+  this.m_arm = m_arm;
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-
-}
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_arm.Axis1EncoderReset();
-    m_arm.resetGetOutputAngle();
-    
-
+    m_arm.Axis2EncoderReset();
+    m_arm.resetAxis2GetOutputAngle(); 
   }
 
   // Called once the command ends or is interrupted.
--- src/main/java/frc/robot/commands/Arm/ResetAxis3Encoder.java
+++ src/main/java/frc/robot/commands/Arm/ResetAxis3Encoder.java
@@ -7,27 +7,23 @@ package frc.robot.commands.Arm;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ArmSubsystem;
 
-public class ResetAxis1Encoder extends CommandBase {
-  /** Creates a new ResetAxis1Encoder. */
+public class ResetAxis3Encoder extends CommandBase {
+  /** Creates a new ResetAxis3Encoder. */
   ArmSubsystem m_arm;
-  public ResetAxis1Encoder(ArmSubsystem m_arm) {
-    this.m_arm = m_arm;
+  public ResetAxis3Encoder(ArmSubsystem m_arm) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.m_arm = m_arm;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-
-}
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_arm.Axis1EncoderReset();
-    m_arm.resetGetOutputAngle();
-    
-
+      m_arm.Axis3EncoderReset();
+   m_arm.resetAxis3GetOutputAngle();  
   }
 
   // Called once the command ends or is interrupted.
