ac2c85956c2dfecf108692b28d9b16d9a819b612
Ç*** F*** <61086421+MorphaxTheDeveloper@users.noreply.github.com>
2023-03-13 12:52:39 +0300

    31

---
+++ src/main/java/frc/robot/commands/Autonomous/AutoHStraightDrive.java
@@ -0,0 +1,26 @@
+package frc.robot.commands.Autonomous;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class AutoHStraightDrive extends PIDCommand {
+
+  public AutoHStraightDrive(DriveSubsystem m_drive, double meters) {
+    super(
+        new PIDController(0.16, 0, 0),
+        () -> m_drive.getHdriveStraightDriveDistance(),
+        () -> meters * 100,
+
+        output -> {
+          m_drive.RunMiddle(-output);
+        });
+
+        getController().setTolerance(3);
+  }
+  
+  @Override
+  public boolean isFinished() {
+    return getController().atSetpoint();
+  }
+}
---
+++ src/main/java/frc/robot/commands/Autonomous/AutoStraightDrive2.java
@@ -0,0 +1,26 @@
+package frc.robot.commands.Autonomous;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class AutoStraightDrive2 extends PIDCommand {
+
+  public AutoStraightDrive2(DriveSubsystem m_drive, double meters) {
+    super(
+        new PIDController(0.16, 0, 0),
+        () -> m_drive.getStraightDriveDistance(),
+        () -> meters * 100,
+
+  output -> {
+    m_drive.arcadeDrive(0, output);
+});
+
+getController().setTolerance(3);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return getController().atSetpoint();
+  }
+}
