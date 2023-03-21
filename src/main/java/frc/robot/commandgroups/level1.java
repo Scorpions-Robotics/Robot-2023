5055532b617be7b31ae08ddbee0bb349701293d5
Ç*** F*** <61086421+MorphaxTheDeveloper@users.noreply.github.com>
2023-03-21 12:23:02 +0300

    new commands

---
+++ src/main/java/frc/robot/commandgroups/level1.java
@@ -0,0 +1,17 @@
+package frc.robot.commandgroups;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.Arm.ArmModeChanger3;
+import frc.robot.commands.Lift.PidLiftModeChanger;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.LiftSubsystem;
+import frc.robot.subsystems.XboxSubsystem;
+
+public class level1 extends SequentialCommandGroup {
+
+  public level1(ArmSubsystem m_arm, LiftSubsystem m_lift, XboxSubsystem m_xbox) {
+    addCommands(
+        new ArmModeChanger3(m_arm, 0, 0, 0)
+            .alongWith(new PidLiftModeChanger(m_lift, () -> 500)));
+  }
+}
---
+++ src/main/java/frc/robot/commands/Arm/ArmModeChanger3.java
@@ -0,0 +1,88 @@
+package frc.robot.commands.Arm;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.XboxSubsystem;
+
+public class ArmModeChanger3 extends CommandBase {
+  ArmSubsystem m_arm;
+  double motor1output;
+  double motor2output;
+  double motor3output;
+
+  double axis1;
+  double axis2;
+  double axis3;
+
+  PIDController controller = new PIDController(0.01, 0, 0);
+  PIDController controller2 = new PIDController(0.025, 0, 0);
+  PIDController controller3 = new PIDController(0.00415, 0, 0);
+
+  public ArmModeChanger3(ArmSubsystem m_arm, double axis1, double axis2, double axis3) {
+    this.m_arm = m_arm;
+
+    this.axis1 = axis1;
+    this.axis2 = axis2;
+    this.axis3 = axis3;
+
+    m_arm.Axis1MotorBreakMode();
+    m_arm.Axis2MotorBreakMode();
+    m_arm.Axis3MotorBreakMode();
+
+    addRequirements(m_arm);
+  }
+
+  @Override
+  public void initialize() {
+  }
+
+  @Override
+  public void execute() {
+
+    controller.setSetpoint(axis1 + 5);
+    controller2.setSetpoint(axis2);
+    controller3.setSetpoint(axis3);
+
+    controller.setTolerance(3);
+    controller2.setTolerance(3);
+    controller3.setTolerance(3);
+
+    motor1output = controller.calculate(-m_arm.getOutputAngle2);
+    motor2output = controller2.calculate(-m_arm.getOutputAngle_Axis2);
+    motor3output = controller3.calculate(-m_arm.getOutputAngle_Axis3);
+
+    SmartDashboard.putNumber("axis3value1234", motor3output);
+
+    if (axis1 + 5 > -m_arm.getOutputAngle2) {
+      m_arm.Axis1MotorOutput(Math.max(-motor1output, -0.20));
+    } else if (-m_arm.getOutputAngle2 > axis1 + 5) {
+      m_arm.Axis1MotorOutput(Math.min(motor1output, 0.20));
+    }
+
+    if (axis2 > m_arm.getOutputAngle_Axis2) {
+      m_arm.Axis2MotorOutput(Math.min(-motor2output, 0.20));
+    } else if (m_arm.getOutputAngle_Axis2 > axis2) {
+      m_arm.Axis2MotorOutput(Math.max(-motor2output, -0.20));
+    }
+
+    if (axis3 > m_arm.getOutputAngle_Axis3) {
+      m_arm.Axis3MotorOutput(Math.max(-motor3output, -0.25));
+
+    } else if (m_arm.getOutputAngle_Axis3 > axis3) {
+      m_arm.Axis3MotorOutput(Math.min(-motor3output, 0.25));
+    }
+
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
