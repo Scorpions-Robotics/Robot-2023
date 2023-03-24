6186b78a3ebc526e737a896cf7d4f8cc82a0c0e2
egeakman <me@egeakman.dev>
2023-03-24 14:26:11 +0300

    hall

---
+++ src/main/java/frc/robot/commandgroups/Autonomous/blue3.java
@@ -0,0 +1,20 @@
+package frc.robot.commandgroups.Autonomous;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.Arm.ArmModeChanger3;
+import frc.robot.commands.Lift.PidLiftCommand;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.LiftSubsystem;
+
+public class blue2 extends SequentialCommandGroup {
+  public blue2(DriveSubsystem m_drive, ArmSubsystem m_arm, LiftSubsystem m_lift) {
+    addCommands(
+        new PidLiftCommand(m_lift, () -> 400)
+            .alongWith(new ArmModeChanger3(m_arm, -18, -130, 0))
+            .withTimeout(2)
+            .andThen(
+                new PidLiftCommand(m_lift, () -> -110)
+                    .alongWith(new ArmModeChanger3(m_arm, -3.5, -2.5, 0))));
+  }
+}
